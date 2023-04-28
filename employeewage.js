5c763e53ace147d55e14c8a0225d1f02e7ea886e
anandsaikumar <anandsai298@gmail.com>
2023-04-28 23:12:05 +0530

    [sai] Add : calculating wage for month of 20 working  days

--- employeewage.js
+++ employeewage.js
@@ -1,17 +1,25 @@
- const  IS_ABSENT = 0;
- const IS_PART_TIME=1;
- const IS_FULL_TIME=2;
+ const IS_ABSENT=0;
+ const  IS_PRESENT = 1;
+ const IS_PART_TIME=2;
+ const IS_FULL_TIME=3;
  const PART_TIME_HRS=4;
  const FULL_TIME_HRS=8;
  const WAGE_PER_HR=20;
+ const NUM_OF_WORKING_DAYS=20;
  let emphrs=0;
- let check = Math.floor(Math.random()*10)%3;
+
+ for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+ {
+    let check = Math.floor(Math.random()*10)%3;
+    emphrs +=getWorkinghours(check);
+ }
  function getWorkinghours(check)
  {
     switch(check)
     {
-        case IS_ABSENT:
-            console.log("Employee IS_ABSENT")
+        case IS_PRESENT:
+            console.log("Employee IS_PRESENT");
+        
         case IS_PART_TIME:
             return PART_TIME_HRS;
         case IS_FULL_TIME:
@@ -20,9 +28,8 @@
             return 0;
      }
  }
- emphrs=getWorkinghours(check);
  let empwage=emphrs*WAGE_PER_HR;
- console.log("empwage"+empwage);
+ console.log("total hrs: "+emphrs+"\n"+"empwage: "+empwage);
 
 
        
