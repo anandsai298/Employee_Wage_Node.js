d275c3e3c0e74e3c5238b84c02639c589eded5c6
anandsaikumar <anandsai298@gmail.com>
2023-04-28 22:00:37 +0530

    [sai] Add : check em[ployee is present or not

---
+++ employeewage.js
@@ -0,0 +1,13 @@
+ const  IS_PRESENt = 1
+ let check = Math.floor(Math.random()*10)%2;
+ if (check == IS_PRESENt)
+ {
+    console.log("Employee IS_PRESENt")
+    return;
+ }
+ else
+ {
+    console.log("Employee is Absent");
+ }
+
+       
